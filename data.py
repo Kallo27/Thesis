f6a3af2326636163b53fa5b945b3ae1530d216ae
Lorenzo Calandra Buonaura <lorenzocb01@gmail.com>
2023-03-31 10:21:31 +0200

    Prova per disegnare i due parametri A(s) e B(s)

---
+++ data.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+data = pd.read_excel(r'.\DWAVE_2000Q_annealing_schedule.xlsx') 
+
+x = list(data['s'])
+y = list(data['A(s) (GHz)'])
+z = list(data['B(s) (GHz)'])
+
+plt.figure(figsize=(10,10))
+plt.style.use('ggplot')
+plt.scatter(x,y,marker=".",s=100,edgecolors="red",c="red")
+plt.scatter(x,z,marker=".",s=100,edgecolors="blue",c="blue")
+plt.title("A(s) e B(s)")
+plt.show()
