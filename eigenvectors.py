02db943c9081c17b9574519db11dc897473df97f
Lorenzo Calandra Buonaura <lorenzocb01@gmail.com>
2023-04-06 12:08:08 +0200

    Prova stampa su excel

---
+++ eigenvectors.py
@@ -0,0 +1,26 @@
+import numpy as np
+import pandas as pd
+
+E = ([[0, -0.5, -0.5, 0],
+    	[-0.5, 1, 0, -0.5],
+    	[-0.5, 0, 1, -0.5],
+    	[0, -0.5, -0.5, 0]])
+
+EigValues, EigVectors = np.linalg.eig(E)
+permute = EigValues.argsort()
+EigValues = EigValues[permute]
+EigVectors = EigVectors[:,permute]
+print(EigVectors)
+
+# Create a dataframe
+data = {
+	"eigenvector": ['e0', 'e1', 'e3', 'e4'],
+	"value": [EigVectors[0], EigVectors[1], EigVectors[2], EigVectors[3]]
+}
+
+df2 = pd.DataFrame(EigVectors, columns=['e0', 'e1', 'e2', 'e3'])
+
+df = pd.DataFrame(data)
+
+# Save the dataframe to an Excel file
+df2.to_excel('output.xlsx', index=False)
