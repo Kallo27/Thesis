89a0f1f1aff68ce00c1ca3cc1a26f41d1c91f873
Lorenzo Calandra Buonaura <lorenzocb01@gmail.com>
2023-04-07 20:00:47 +0200

    Added program to save the eigenvectors to an excel file

---
+++ eigvec_to_excel.py
@@ -0,0 +1,75 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Pauli matrices and identity
+
+s1 = np.matrix([[0,1],[1,0]])
+s2 = np.matrix([[0,-1j],[1j,0]])
+s3 = np.matrix([[1,0],[0,-1]])
+I = np.matrix([[1, 0],[0,1]])
+
+# tensor product
+
+s1_I = np.kron(s1, I)
+I_s1 = np.kron(I, s1)
+s3_I = np.kron(s3, I)
+I_s3 = np.kron(I, s3)
+s3_s3 = np.kron(s3, s3)
+
+def truncate(n, decimals=0):
+    multiplier = 10 ** decimals
+    return int(n * multiplier) / multiplier
+
+# objective function parameters
+
+def coupling_strengths(t):
+    return t
+
+def biases(t):
+    return t
+
+# QPU anneal parameters
+
+data = pd.read_excel(r'.\DWAVE_2000Q_annealing_schedule.xlsx')
+A = np.array(data['A(s) (GHz)'])
+B = np.array(data['B(s) (GHz)'])
+s = np.array(data['s'])
+
+# hamiltonians
+
+h0 = (s1_I + I_s1)
+
+def hf(h, J):
+    return (biases(h) * (s3_I + I_s3)) + (coupling_strengths(J) * s3_s3)
+
+def H(t, h, J):
+    return - A.item(t) / 2 * h0 + B.item(t) / 2 * hf(h, J)
+    
+# eigenvalues and eigenvectors
+
+E = np.empty([4, 4])
+h_values = np.empty([4, 4])
+J_values = np.empty([4, 4])
+
+for h in range (-10, 10):
+    for J in range (-10, 10): 
+        for i in range (0, len(s)):
+            EigValues, EigVectors = np.linalg.eig(H(i, h/10, J/10))
+            permute = EigValues.argsort()
+            EigValues = EigValues[permute]
+            EigVectors = EigVectors[:,permute]
+            
+            EigVectors = np.real(EigVectors)
+                
+            for pluto in range (0,4):
+                truncate(EigVectors.item(pluto), 2)
+
+        E = np.append(E, EigVectors, axis = 0)
+
+for i in range (0, 4): 
+    E = np.delete(E, 0, 0)
+
+data = pd.DataFrame(E, columns=['e0', 'e1', 'e2', 'e3'])
+
+data.to_excel('eigenvectors.xlsx', index=False)
