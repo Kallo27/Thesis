0823b32bb28c58c6c81a323b23156b4a21d77743
Lorenzo Calandra Buonaura <lorenzocb01@gmail.com>
2023-05-22 16:05:08 +0000

    Prove su chimera

---
+++ kp2.py
@@ -0,0 +1,10 @@
+from dwave.system import DWaveSampler, AutoEmbeddingComposite
+import dwave.inspector
+sampler = AutoEmbeddingComposite(DWaveSampler(solver={'topology__type': 'chimera'}))
+
+h = {'s_1': 0.5, 's_2': 0.0, 's_3': 1.0}
+J = {('s_1', 's_2'): 1.5, ('s_2', 's_3'): 1.5, ('s_1', 's_3'): 1.5}
+sampleset = sampler.sample_ising(h, J, num_reads=2000, label='knapsack problem - 2 objects')
+print(sampleset)
+print(sampleset.info)
+dwave.inspector.show(sampleset)
