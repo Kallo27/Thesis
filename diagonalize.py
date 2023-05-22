d200b182bcd95434eb10af2abaebc12152d21437
Lorenzo Calandra Buonaura <lorenzocb01@gmail.com>
2023-05-22 18:06:29 +0200

    Updates

---
+++ diagonalize.py
@@ -0,0 +1,12 @@
+from numpy import *
+M = Matrix([[3, -2,  4, -2],
+                                [5,  3, -3, -2],
+                                [5, -2,  2, -2],
+                                [5, -2, -3,  3]])
+  
+print("Matrix : {} ".format(M))
+   
+# Use sympy.diagonalize() method 
+P, D = M.diagonalize()  
+      
+print("Diagonal of a matrix : {}".format(D))  
