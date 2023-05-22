c588a1a4d38536448598048b2a0615a3703f94e5
Lorenzo Calandra Buonaura <lorenzocb01@gmail.com>
2023-05-22 23:52:55 +0200

    Tries

--- three_four_comparison.py
+++ three_four_comparison.py
@@ -46,7 +46,7 @@ coupling_strengths3 = np.matrix([[0, 1.5, 1.5],
                                 [0, 0, 0]])
 
 biases4 = np.array([0.5, 0, 1, 0.5])
-coupling_strengths4 = np.matrix([[0, 1.5, 0, -5], 
+coupling_strengths4 = np.matrix([[0, 1.5, 0, -3], 
                                 [0, 0, 1.5, 0],
                                 [0, 0, 0, 1.5], 
                                 [0, 0, 0, 0]])
