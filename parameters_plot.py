d1b7cc71d5bcafd6ebaab88159af12f0a2c35707
Lorenzo Calandra Buonaura <lorenzocb01@gmail.com>
2023-06-09 12:46:06 +0200

    Added legend

--- parameters_plot.py
+++ parameters_plot.py
@@ -7,12 +7,17 @@ A = np.array(data['A(s) (GHz)'])
 B = np.array(data['B(s) (GHz)'])
 s = np.array(data['s'])
 
+h = 6.62607015e-25
+
+A = A * h
+B = B * h
 # drawing
 
 plt.grid()
 A_s, = plt.plot(s, A, c = 'blue', label = 'A(s)')
 B_s, = plt.plot(s, B, c = 'red', label = 'B(s)')
 plt.xlabel("s = t/tA")
-plt.ylabel("Energy (GHz)")
+plt.ylabel("Energy (J)")
 plt.legend(handles=[A_s, B_s])
 plt.show()
+print(h)
