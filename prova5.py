bbce5a99e37f6cf4c6c34d2d02a2008cf45e316e
Lorenzo Calandra Buonaura <lorenzocb01@gmail.com>
2023-06-14 16:51:17 +0000

    Added inspector

--- prova5.py
+++ prova5.py
@@ -1,6 +1,6 @@
 from dwave.system import DWaveSampler, AutoEmbeddingComposite
 import dwave.inspector
-sampler = AutoEmbeddingComposite(DWaveSampler(solver={'topology__type': 'chimera'}))
+sampler = AutoEmbeddingComposite(DWaveSampler(solver={'topology__type': 'pegasus'}))
 
 h = {'s_1': 0.5, 's_2': 0.0, 's_3': 1.0, 's_4': 2.0, 's_5': 1.0}
 J = {('s_1', 's_2'): 1.5, ('s_2', 's_3'): 1.5, ('s_1', 's_3'): 1.5, ('s_1', 's_4'): 1.5, ('s_1', 's_5'): 1.5, 
