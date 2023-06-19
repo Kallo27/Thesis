2eb4a452cc4192286764af5c0215c7ef46b7b344
Lorenzo Calandra Buonaura <lorenzocb01@gmail.com>
2023-06-19 15:28:36 +0000

    Complete graphs: 4 and 20 qubits

---
+++ complete_graph_4.py
@@ -0,0 +1,14 @@
+from dwave.system import DWaveSampler, AutoEmbeddingComposite
+import dwave.inspector
+sampler = AutoEmbeddingComposite(DWaveSampler(solver={'topology__type': 'pegasus'}))
+
+h = {'s_1': 9/2, 's_2': 4.0, 's_3': 7/2, 's_4': 5.0}
+J = {('s_1', 's_2'): 2.5, ('s_1', 's_3'): 2.5, ('s_1', 's_4'): 2.5, ('s_2', 's_3'): 2.5, 
+    ('s_2', 's_4'): 2.5, ('s_3', 's_4'): 2.5}
+
+sampleset = sampler.sample_ising(h, J, num_reads=2000, label='knapsack problem - 3 objects')
+
+print(sampleset)
+print(sampleset.info)
+
+dwave.inspector.show(sampleset)
