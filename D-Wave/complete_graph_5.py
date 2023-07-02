b3796a606d767794d773d3c2f34f24424fb13b3d
Lorenzo Calandra Buonaura <lorenzocb01@gmail.com>
2023-07-02 22:34:22 +0000

    5000 reads

--- D-Wave/complete_graph_5.py
+++ D-Wave/complete_graph_5.py
@@ -6,7 +6,7 @@ h = {'s_1': 7.0, 's_2': 13/2, 's_3': 6.0, 's_4': 11/2, 's_5': 15/2}
 J = {('s_1', 's_2'): 2.5, ('s_2', 's_3'): 2.5, ('s_1', 's_3'): 2.5, ('s_1', 's_4'): 2.5, ('s_1', 's_5'): 2.5, 
     ('s_2', 's_4'): 2.5, ('s_2', 's_5'): 2.5,('s_3', 's_4'): 2.5, ('s_3', 's_5'): 2.5, ('s_4', 's_5'): 2.5}
 
-sampleset = sampler.sample_ising(h, J, num_reads=2000, label='knapsack problem - 4 objects')
+sampleset = sampler.sample_ising(h, J, num_reads=5000, label='knapsack problem - 4 objects')
 
 print(sampleset)
 print(sampleset.info)
