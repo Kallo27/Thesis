32d432a969996d9c3925c3a428b7679d0705af44
Lorenzo Calandra Buonaura <lorenzocb01@gmail.com>
2023-07-03 17:29:35 +0000

    kp2

--- D-Wave/complete_graph_2.py
+++ D-Wave/complete_graph_2.py
@@ -2,8 +2,8 @@ from dwave.system import DWaveSampler, AutoEmbeddingComposite
 import dwave.inspector
 sampler = AutoEmbeddingComposite(DWaveSampler(solver={'topology__type': 'pegasus'}))
 
-h = {'s_1': -5, 's_2': -5}
-J = {('s_1', 's_2'): 2.5}
+h = {'s_1': 0, 's_2': 1}
+J = {('s_1', 's_2'): 0.5}
 
 sampleset = sampler.sample_ising(h, J, num_reads=5000, label='knapsack problem - 1 object')
 
